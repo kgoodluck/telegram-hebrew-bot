f593584b02e5cd1016a0bf45039f42fbf702b981
kgoodluck <k-goodluck@yandex.ru>
2025-02-11 17:58:24 +0200

    add redis

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import bodyParser from "body-parser";
+import { Redis } from "@telegraf/session/redis";
 import { Telegraf, session } from "telegraf";
 import { startBot } from "./src/botHandlers/startBot.js";
 import { handleUserInput } from "./src/botHandlers/handleUserInput.js";
@@ -9,8 +10,10 @@ import { applyGlobalErrorHandler } from "./src/botHandlers/errorHandler.js";
 const app = express();
 app.use(bodyParser.json());
 
+const store = Redis({ url: process.env.REDIS_URL });
+
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
-bot.use(session());
+bot.use(session({ store }));
 bot.start((ctx) => startBot(ctx));
 bot.on("text", async (ctx) => handleUserInput(ctx));
 bot.launch();
--- src/botHandlers/startBot.js
+++ src/botHandlers/startBot.js
@@ -1,6 +1,3 @@
-import { resetSession } from "../sessionManager.js";
-
 export function startBot(ctx) {
-    resetSession(ctx);
     ctx.reply("How many sentences do you want? (e.g., 5)");
 }
