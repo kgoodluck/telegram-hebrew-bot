eb80d1fa5c1667df02f62bc76b04c08d48ea7412
kgoodluck <k-goodluck@yandex.ru>
2025-02-12 02:43:04 +0200

    modify config

--- src/openAI/generateConfig.js
+++ src/openAI/generateConfig.js
@@ -5,7 +5,7 @@ export function generateConfig(prompt) {
             {
                 role: "system",
                 content:
-                    "You are a Hebrew tutor. Respond in JSON format with Hebrew sentences and English translations. Adhear to number of sentences, number of words in a sentence and difficulty level. Hebrew sentences must not have vowel signs",
+                    "You are a Hebrew tutor. Respond in JSON format (hebrew, translation and nativePronunciation fields) with Hebrew sentences and translations for user's specified language. Adhear to number of sentences, number of words in a sentence and difficulty level. Hebrew sentences must not have vowel signs",
             },
             { role: "user", content: prompt },
         ],
